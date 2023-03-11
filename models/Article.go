661443b3497c42fd3aea5a063fa3693625a4ff1c
linhx1999 <linhx1999@163.com>
2023-03-11 22:00:21 +0800

    Change cid to category_id

--- models/Article.go
+++ models/Article.go
@@ -3,11 +3,11 @@ package models
 import "gorm.io/gorm"
 
 type Article struct {
-	Category Category `gorm:"foreignKey:CID"`
+	Category Category `gorm:"foreignKey:CategoryID"`
 	gorm.Model
-	Title   string `gorm:"type:varchar(100);not null" json:"title"`
-	CID     int    `gorm:"type:int;not null" json:"cid"`
-	Desc    string `gorm:"type:varchar(200)" json:"desc"`
-	Content string `gorm:"type:longtext" json:"content"`
-	Img     string `gorm:"type:varchar(100)" json:"img"`
+	Title      string `gorm:"type:varchar(100);not null" json:"title"`
+	CategoryID int    `gorm:"type:int;not null" json:"category_id"`
+	Desc       string `gorm:"type:varchar(200)" json:"desc"`
+	Content    string `gorm:"type:longtext" json:"content"`
+	Img        string `gorm:"type:varchar(100)" json:"img"`
 }
