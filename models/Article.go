7d0fd930b5f740b5c7c3c8ebd7becf267094f7ba
linhx1999 <linhx1999@163.com>
2023-03-12 10:35:26 +0800

    Change json:category_id to categoryID

--- models/Article.go
+++ models/Article.go
@@ -6,7 +6,7 @@ type Article struct {
 	Category Category `gorm:"foreignKey:CategoryID"`
 	gorm.Model
 	Title      string `gorm:"type:varchar(100);not null" json:"title"`
-	CategoryID int    `gorm:"type:int;not null" json:"category_id"`
+	CategoryID int    `gorm:"type:int;not null" json:"categoryID"`
 	Desc       string `gorm:"type:varchar(200)" json:"desc"`
 	Content    string `gorm:"type:longtext" json:"content"`
 	Img        string `gorm:"type:varchar(100)" json:"img"`
